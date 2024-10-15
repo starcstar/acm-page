8dd4058a32d06c3e65886c8f061f408041acaaff
4627488 <h4627488@outlook.com>
2024-10-15 15:38:21 +0800

    Update deploy.yml to use Azure static web apps deploy action

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,12 @@
 import DefaultTheme from 'vitepress/theme';
 import './styles/custom.css';
+import googleAnalytics from 'vitepress-plugin-google-analytics'
 
-export default DefaultTheme;
+export default {
+    ...DefaultTheme,
+    enhanceApp: (ctx) => {
+        googleAnalytics({
+            id: 'GTM-KVJF8W5L', // Replace with your GoogleAnalytics ID, which should start with the 'G-'
+        })
+    }
+}
