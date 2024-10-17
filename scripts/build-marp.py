463391bf70172cc412836e346de94c2a7e4eefcf
4627488 <h4627488@outlook.com>
2024-10-17 18:10:37 +0800

    Update .gitignore to include public/2024fall directory

---
+++ scripts/build-marp.py
@@ -0,0 +1,52 @@
+import os
+import subprocess
+from glob import glob
+
+# 指定搜索的根目录
+SEARCH_DIR = '2024fall'
+# 指定输出的根目录
+OUTPUT_DIR = os.path.join('public', SEARCH_DIR)
+
+# 查找所有名为slide.md的文件
+files = glob(f'{SEARCH_DIR}/**/slide.md', recursive=True)
+
+
+def compile_file(file, output_dir, format):
+    output_file_path = os.path.join(output_dir, f'slide.{format}')
+    # 读取frontmatter yaml
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        if lines[0].startswith('---'):
+            frontmatter_end = lines.index('---\n', 1)
+            frontmatter = ''.join(lines[:frontmatter_end + 1])
+            lines = lines[frontmatter_end + 1:]
+        else:
+            frontmatter = ''
+        # 解析theme的值
+        theme = 'default'
+        for line in frontmatter.split('\n'):
+            if line.startswith('theme:'):
+                theme = line.split(':')[-1].strip()
+                break
+    if theme == 'default':
+        command = f'npx @marp-team/marp-cli@latest "{file}" -o "{output_file_path}"'
+    else:
+        command = f'npx @marp-team/marp-cli@latest --theme ./marp-themes/{theme}.css "{file}" -o "{output_file_path}"'
+    print(f'Running command: {command}')
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    if result.returncode != 0:
+        print(f'Error converting {file} to {format.upper()}: {result.stderr}')
+    else:
+        print(f'Successfully converted {file} to {output_file_path}')
+
+for file in files:
+    # 获取文件的相对路径
+    relative_path = os.path.relpath(file, SEARCH_DIR)
+    # 获取文件所在的目录
+    dir_path = os.path.dirname(relative_path)
+    # 创建对应的输出目录
+    output_dir = os.path.join(OUTPUT_DIR, dir_path)
+    os.makedirs(output_dir, exist_ok=True)
+
+    compile_file(file, output_dir, 'pdf')
+    compile_file(file, output_dir, 'html')
